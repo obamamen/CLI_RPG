dd40ce1880455394c279f58cb4ad22d8f11a2209
Poesgamer <Moosmegens@gmail.com>
2024-07-26 16:39:59 +0200

    start

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>  // For _kbhit() and _getch() on Windows
+
+
+#define WORLD_WIDTH 50
+#define WORLD_HEIGHT 50
+#define VIEW_WIDTH 10
+#define VIEW_HEIGHT 10
+
+typedef struct {
+    int x;
+    int y;
+} Entity;
+
+int main () {
+
+}
